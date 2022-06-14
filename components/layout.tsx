1123b323f77a11737a4ff88ce3193978105272e7
KelvinChim-ComSci <kelvin2000cc@gmail.com>
2022-06-14 13:19:58 +0800

    add transition and bg color

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Layout({ children }: any) {
   const router = useRouter();
@@ -22,20 +23,46 @@ export default function Layout({ children }: any) {
     },
   ];
 
+  useEffect(() => {
+    const overlay: HTMLElement = document.getElementById("overlay")!;
+    const navItems: NodeListOf<Element> =
+      document.querySelectorAll("nav ul li")!;
+
+    navItems.forEach((item) => {
+      item.addEventListener("mouseover", (e) => {
+        e.preventDefault();
+        overlay.classList.add("active");
+        let position = item.getBoundingClientRect();
+        overlay.style.top = position.y.toString() + "px";
+        overlay.style.left = position.x.toString() + "px";
+        overlay.style.height = position.height.toString() + "px";
+        overlay.style.width = position.width.toString() + "px";
+      });
+
+      item.addEventListener("mouseout", () => {
+        overlay?.classList.remove("active");
+      });
+    });
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="flex flex-col md:flex-row flex-1">
-        <aside className="bg-gradient-to-r from-gray-200 w-full md:w-80">
-          <nav>
+        <aside className="bg-slate-200 opacity-50 w-full md:w-80">
+          <nav className="mt-24">
+            <div id="overlay" className="-z-10 absolute bg-slate-400"></div>
             <ul>
               {menuItems.map(({ href, title }) => (
-                <li className="m-2 ml-0" key={title}>
+                <li
+                  className={`p-2 pl-0 ${
+                    router.asPath === href &&
+                    "bg-gradient-to-r from-gray-400 to-slate-300 text-white"
+                  }`}
+                  key={title}
+                >
                   <Link href={href}>
                     <a
-                      className={`flex px-8 py-2 hover:bg-gradient-to-r gray-400 cursor-pointer ${
-                        router.asPath === href &&
-                        "bg-gradient-to-r from-gray-400 to-white text-white"
-                      }`}
+                      className={`flex px-8 py-2 hover:bg-gradient-to-r gray-400 cursor-pointer `}
                     >
                       {title}
                     </a>
@@ -45,9 +72,9 @@ export default function Layout({ children }: any) {
             </ul>
           </nav>
         </aside>
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 bg-slate-50">{children}</main>
       </div>
-      <footer className="py-2 bg-gray-300">Copyright somthing</footer>
+      <footer className="py-2 bg-gray-300/50">Copyright somthing</footer>
     </div>
   );
 }
