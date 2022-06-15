7c7b8c25416e48470006bc2c459478c52089e811
KelvinChim-ComSci <kelvin2000cc@gmail.com>
2022-06-15 16:34:49 +0800

    added dark mode

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useTheme } from "next-themes";
 import { useEffect } from "react";
 
 export default function Layout({ children }: any) {
   const router = useRouter();
+  const { theme, setTheme } = useTheme();
   const menuItems: { href: string; title: string }[] = [
     {
       href: "/",
@@ -48,7 +50,10 @@ export default function Layout({ children }: any) {
   return (
     <div className="min-h-screen flex flex-col">
       <div className="flex flex-col md:flex-row flex-1">
-        <aside className="bg-neutral-200 opacity-50 w-full md:w-80">
+        <aside className="bg-zinc-300 dark:bg-slate-800 opacity-50 w-full md:w-80">
+          <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+            toggle
+          </button>
           <nav className="mt-24">
             <div id="overlay" className="-z-10 absolute bg-neutral-300"></div>
             <ul>
@@ -62,7 +67,7 @@ export default function Layout({ children }: any) {
                 >
                   <Link href={href}>
                     <a
-                      className={`select-none flex px-8 py-2 font-semibold cursor-pointer `}
+                      className={`select-none flex px-8 py-2 font-semibold text-black dark:text-white cursor-pointer `}
                     >
                       {title}
                     </a>
@@ -72,10 +77,10 @@ export default function Layout({ children }: any) {
             </ul>
           </nav>
         </aside>
-        <main className="flex-1 bg-slate-50">{children}</main>
+        <main className="flex-1 bg-zinc-50 dark:bg-slate-800">{children}</main>
       </div>
-      <footer className="select-none p-2 bg-gray-300/50 text-right text-sm text-neutral-400">
-        &copy; Created by Kelvin Chim. Last Updated at: 14/6/2022.
+      <footer className="select-none p-2 bg-zinc-400 dark:bg-slate-700 text-right text-sm text-black dark:text-white">
+        &copy; Created by Kelvin Chim. Last Updated at: 15/6/2022.
       </footer>
     </div>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
 import { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
 import Head from "next/head";
 import Layout from "../components/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Head>
-        <title>Among us</title>
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider attribute="class">
+      <Layout>
+        <Head>
+          <title>Among us</title>
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
 export default MyApp;
