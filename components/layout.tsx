44c92702cf74b30dfab1e223ecfa2d73e47ed7dc
KelvinChim-ComSci <kelvin2000cc@gmail.com>
2022-06-16 18:21:48 +0800

    experiencing responsive design

--- components/layout.tsx
+++ components/layout.tsx
@@ -49,17 +49,14 @@ export default function Layout({ children }: any) {
 
   return (
     <div className="min-h-screen flex flex-col">
-      <div className="flex flex-col md:flex-row flex-1">
-        <aside className="bg-zinc-300 dark:bg-slate-800 opacity-50 w-full md:w-80">
-          <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-            toggle
-          </button>
-          <nav className="mt-24">
+      <div className="flex flex-col-reverse sm:flex-row flex-1">
+        <aside className="bg-zinc-300 dark:bg-slate-800 opacity-50 w-full sm:w-80">
+          <nav className="mt-0 sm:mt-24">
             <div id="overlay" className="-z-10 absolute bg-neutral-300"></div>
             <ul>
               {menuItems.map(({ href, title }) => (
                 <li
-                  className={`p-2 pl-0 font-semibold ${
+                  className={`p-2 pl-0 font-semibold float-left sm:float-none ${
                     router.asPath === href &&
                     "bg-gradient-to-r from-neutral-400 to-neutral-300"
                   }`}
@@ -76,10 +73,17 @@ export default function Layout({ children }: any) {
               ))}
             </ul>
           </nav>
+          <div>
+            <button
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            >
+              toggle
+            </button>
+          </div>
         </aside>
         <main className="flex-1 bg-zinc-50 dark:bg-slate-800">{children}</main>
       </div>
-      <footer className="select-none p-2 bg-zinc-400 dark:bg-slate-700 text-right text-sm text-black dark:text-white">
+      <footer className="select-none p-2 bg-zinc-400 dark:bg-slate-700 text-right text-sm text-black text-opacity-50 dark:text-white dark:text-opacity-50">
         &copy; Created by Kelvin Chim. Last Updated at: 15/6/2022.
       </footer>
     </div>
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,7 +2,12 @@ export default function Contact() {
   return (
     <div className="flex h-full flex-col justify-center items-center">
       <h1 className="text-4xl mb-5 font-bold">Contact</h1>
-      <span className="text-7xl">📞</span>
+      <div className="text-8xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        nostrum atque dolorum fugiat laborum, ducimus nemo architecto magnam
+        ipsum dicta corrupti, sapiente aperiam, nisi nulla illo sequi dolore
+        mollitia eius?
+      </div>
     </div>
   );
 }
