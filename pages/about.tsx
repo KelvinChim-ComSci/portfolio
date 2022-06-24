096d179da9044ad28594739cfd374ed0ad99ef6e
KelvinChim-ComSci <kelvin2000cc@gmail.com>
2022-06-24 18:05:28 +0800

    about me structure init

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,12 +3,14 @@ import { AiFillGithub } from "react-icons/ai";
 
 export default function About() {
   return (
-    <div className="flex h-full flex-col justify-center items-center">
-      <h1 className="text-4xl mb-5 font-bold">About</h1>
-      <span className="text-7xl">💬</span>
-      <Link href="https://github.com/KelvinChim-ComSci/">
-        <AiFillGithub size={"2rem"} className="cursor-pointer" />
-      </Link>
+    <div className="flex h-full flex-col p-6">
+      <h1 className="text-4xl mb-5 font-bold">About Me: </h1>
+      <div id="backgroundInfo" className="flex grow"></div>
+      <div id="relatedFields" className="flex grow">
+        <Link href="https://github.com/KelvinChim-ComSci/">
+          <AiFillGithub size={"2rem"} className="cursor-pointer" />
+        </Link>
+      </div>
     </div>
   );
 }
