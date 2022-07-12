15e4b8e4d67ee27b689813ed70d58883b913f5d5
KelvinChim-ComSci <kelvin2000cc@gmail.com>
2022-07-12 16:19:37 +0800

    added content idk

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,8 +4,12 @@ import { AiFillGithub } from "react-icons/ai";
 export default function About() {
   return (
     <div className="flex h-full flex-col p-6">
-      <h1 className="text-4xl mb-5 font-bold">About Me: </h1>
-      <div id="backgroundInfo" className="flex grow"></div>
+      <h1 className="text-4xl mb-5 font-bold">About Me:</h1>
+      <div id="backgroundInfo" className="flex grow">
+        I am a software developer just graduated from University. Currently
+        working as a full-time frontend developer, I wish one day I can work in
+        the gaming industry and develop memorable games in the future.
+      </div>
       <div id="relatedFields" className="flex grow">
         <Link href="https://github.com/KelvinChim-ComSci/">
           <AiFillGithub size={"2rem"} className="cursor-pointer" />
--- pages/work.tsx
+++ pages/work.tsx
@@ -2,7 +2,19 @@ export default function Work() {
   return (
     <div className="flex h-full flex-col justify-center items-center">
       <h1 className="text-4xl mb-5 font-bold">Work</h1>
-      <span className="text-7xl">work</span>
+      <span className="text-l">
+        Previous Experience:
+        <ul>
+          <li>
+            Major in Mathematics, Computation and Applied Mathematics Stream
+            <br />
+            Minor in Computer Science
+            <br />
+            The Chinese University of Hong Kong
+          </li>
+          <li></li>
+        </ul>
+      </span>
     </div>
   );
 }
