66a8cd9e43a865b27635a31f901d4497289d81ce
KelvinChim-ComSci <kelvin2000cc@gmail.com>
2022-07-18 17:31:03 +0800

    works layout in web version done

--- components/workForm.tsx
+++ components/workForm.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Image from "next/image";
 import { work } from "../database/workItems";
 
 interface Props {
@@ -9,23 +9,36 @@ export default function WorkForm(props: Props): JSX.Element {
   const title = props.workItem.title;
   const link = props.workItem.link;
   const description = props.workItem.description;
+  const image = props.workItem.img;
 
+  function ImageComponent(image: string) {
+    return (
+      <Image
+        src={`/static/images/${image}.png`}
+        width="100%"
+        height="100%"
+        layout="fill"
+        objectPosition="right"
+        objectFit="contain"
+      />
+    );
+  }
   return (
     <div className="border border-sky-500 p-3 h-64 mb-8 flex flex-row">
-      <div className="textDiv basis-3/4 flex flex-col relative">
+      <div className="textDiv basis-3/5 flex flex-col relative">
         <h3 className="text-2xl font-bold basis-1/5">{title}</h3>
         <div className="basis-3/5">{description}</div>
         <div className="absolute basis-1/5 bottom-0 left-0">
           <div>
             Check out my work here:&nbsp;
-            <Link href={link}>
+            <a href={link.href}>
               <span className="cursor-pointer text-sky-600">{title}</span>
-            </Link>
+            </a>
           </div>
         </div>
       </div>
-      <div className="imageDiv basis-1/4">
-        <h3></h3>
+      <div className="imageDiv basis-2/5 relative">
+        {image ? ImageComponent(image) : ""}
       </div>
     </div>
   );
--- database/workItems.tsx
+++ database/workItems.tsx
@@ -2,14 +2,15 @@ export interface work {
   title: string;
   link: URL;
   description: string;
-  img?: any;
+  img?: string;
 }
 
 export const workItems: work[] = [
   {
     title: "CU Simulator",
     link: new URL("https://csci3100-game.herokuapp.com"),
-    description:
-      "A Point-and-click RPG game about the University life of a student, created as a school project by me and my groupmates.",
+    description: `A Point-and-click RPG game about the University life of a student, created as a school project by me and my groupmates. 
+      The project utilized MERN stack and various node modules to create a simple web game, which also includes instant messaging.`,
+    img: "CUSimulator",
   },
 ];
